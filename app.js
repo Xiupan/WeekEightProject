7788df21fb6938f58a85a2b0bb27914a1442346c
Alan Hong <xiupan@gmail.com>
2017-07-14 09:59:15 -0500

    Layout setup.

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ mongoose.connect('mongodb://localhost:27017/codesnippetDB'); // need to be chang
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+app.set('layout', 'layout');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
