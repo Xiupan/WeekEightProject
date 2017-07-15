d4a87c920dac3fdce23ff79130ad6298b734a090
Alan Hong <xiupan@gmail.com>
2017-07-15 14:31:44 -0500

    Added comments to decode body.

--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
 var encodedBodyArr = document.querySelectorAll('.body-encoded');
-for (var i = 0; i < encodedBodyArr.length; i++) {
+for (var i = 0; i < encodedBodyArr.length; i++) { // Decodes the encoded body text and displays properly
   var decodedBody = decodeURI(encodedBodyArr[i].innerHTML);
   encodedBodyArr[i].innerHTML = decodedBody;
 }
